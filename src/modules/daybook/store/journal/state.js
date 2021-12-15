6231a94b90661c6fa7f143b3532fc50d788a73b1
tonocp <amcp88@gmail.com>
2021-12-15 12:22:53 +0100

    Journal_App

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
   isLoading: true,
-  entries: [],
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toLocaleDateString(),
+      text: "Sit excepteur id consectetur dolore amet dolore qui laboris eu nulla irure. Esse amet pariatur enim veniam proident ad proident. Adipisicing laborum laboris ea elit. Proident duis nulla officia esse culpa ullamco aute anim ut duis tempor.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toLocaleDateString(),
+      text: "Culpa adipisicing amet enim anim cupidatat qui mollit nisi labore culpa tempor adipisicing ut est. Consectetur eu ad non adipisicing aliqua aliqua aliquip minim consequat officia sunt cupidatat veniam esse. Reprehenderit fugiat minim consequat adipisicing irure. Ea eiusmod duis incididunt non eiusmod laboris id qui deserunt velit pariatur velit. Commodo aliqua in id incididunt et enim ea dolor cillum sunt elit id pariatur eiusmod. Ipsum laborum eiusmod incididunt reprehenderit deserunt quis in irure dolor nisi laboris culpa ad esse.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toLocaleDateString(),
+      text: "Anim amet labore labore aliqua commodo irure duis veniam ipsum Lorem est est sit. In culpa quis exercitation id proident. Ea irure occaecat labore consequat nulla irure commodo eu labore quis eu dolor Lorem sint. Deserunt deserunt ad pariatur laboris enim aliqua.",
+      picture: null,
+    },
+  ],
 });
