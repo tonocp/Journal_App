9021711249a1db2d20014a4da6da86816713bb50
tonocp <amcp88@gmail.com>
2021-12-15 10:41:43 +0100

    first commit

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="btn btn-primary">
+    <i class="fa fa-2x fa-plus"></i>
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+button {
+  border-radius: 100%;
+  bottom: 20px;
+  height: 70px;
+  position: fixed;
+  right: 20px;
+  width: 70px;
+}
+</style>
