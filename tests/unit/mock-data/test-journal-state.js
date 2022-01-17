15cb4fa856942ba831b65423fa28171e5a9344c2
tonocp <amcp88@gmail.com>
2022-01-17 14:30:52 +0100

    Unit Testing VUEX

---
+++ tests/unit/mock-data/test-journal-state.js
@@ -0,0 +1,15 @@
+export const journalState = {
+  isLoading: false,
+  entries: [
+    {
+      id: "-Mr2XS8HZ41T8-mAxch7",
+      date: 1639663113422,
+      text: "Hola mundo desde mock data",
+    },
+    {
+      id: "-Mtc1XLEhm5SJWiTNHCM",
+      date: 1642422993873,
+      text: "Hola mundo desde mock data2",
+    },
+  ],
+};
---
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -0,0 +1,94 @@
+import { createStore } from "vuex";
+import journal from "@/modules/daybook/store/journal";
+import { journalState } from "../../../../mock-data/test-journal-state";
+
+const createVuexStore = (initialState) =>
+  createStore({
+    modules: {
+      journal: {
+        ...journal,
+        state: {
+          ...initialState,
+        },
+      },
+    },
+  });
+
+describe("VUEX - Pruebas en el Journal Module", () => {
+  // BÁSICAS *********************************************
+
+  test("Debe tener este estado inicial", () => {
+    const store = createVuexStore(journalState);
+    const { isLoading, entries } = store.state.journal;
+
+    expect(isLoading).toBeFalsy();
+    expect(entries).toEqual(journalState.entries);
+  });
+
+  // MUTATIONS *********************************************
+
+  test("Mutation: setEntries", () => {
+    const store = createVuexStore({ isLoading: true, entries: [] });
+
+    store.commit("journal/setEntries", journalState.entries);
+    expect(store.state.journal.entries.length).toBe(2);
+
+    store.commit("journal/setEntries", journalState.entries);
+    expect(store.state.journal.entries.length).toBe(4);
+
+    expect(store.state.journal.isLoading).toBeFalsy();
+  });
+
+  test("Mutation: updateEntry", () => {
+    const store = createVuexStore(journalState);
+    const updatedEntry = {
+      id: "-Mr2XS8HZ41T8-mAxch7",
+      date: 1639663113422,
+      text: "Hola mundo desde pruebas",
+    };
+
+    store.commit("journal/updateEntry", updatedEntry);
+
+    const storeEntries = store.state.journal.entries;
+
+    expect(storeEntries.length).toBe(2);
+    expect(storeEntries.find((e) => e.id === updatedEntry.id)).toEqual(updatedEntry);
+  });
+
+  test("Mutation: addEntry & deleteEntry", () => {
+    const store = createVuexStore(journalState);
+
+    store.commit("journal/addEntry", { id: "ABC-123", text: "Hola Pruebas" });
+
+    expect(store.state.journal.entries.length).toBe(3);
+    expect(store.state.journal.entries.find((e) => e.id === "ABC-123")).toBeTruthy();
+
+    store.commit("journal/deleteEntry", "ABC-123");
+    expect(store.state.journal.entries.length).toBe(2);
+    expect(store.state.journal.entries.find((e) => e.id === "ABC-123")).toBeFalsy();
+  });
+
+  // GETTERS *********************************************
+
+  test("Getters: getEntriesByTerm getEntryById", () => {
+    const store = createVuexStore(journalState);
+
+    const [entry1, entry2] = journalState.entries;
+
+    expect(store.getters["journal/getEntriesByTerm"]("").length).toBe(2);
+    expect(store.getters["journal/getEntriesByTerm"]("2").length).toBe(1);
+    expect(store.getters["journal/getEntriesByTerm"]("2")).toEqual([entry2]);
+
+    expect(store.getters["journal/getEntryById"](entry1.id)).toEqual(entry1);
+  });
+
+  // ACTIONS *********************************************
+
+  test("Actions: loadEntries", async () => {
+    const store = createVuexStore({ isLoading: true, entries: [] });
+
+    await store.dispatch("journal/loadEntries");
+
+    expect(store.state.journal.entries.length).toBe(2);
+  });
+});
